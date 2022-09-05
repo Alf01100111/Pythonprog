fb7e3da789dfa068d653529403d140d3361f37de
Андрей хухарев <alf.master.mail@gmail.com>
2022-09-05 22:28:07 +0300

    task 3

---
+++ task 3.py
@@ -0,0 +1,14 @@
+# Задайте список из k чисел последовательности (1 + 1\k)^k и выведите на экран их сумму.
+import array
+
+k = int(input("Введите К "))
+
+array = []
+sum = 0
+
+for i in range(1,k+1):
+    array.append((1+1/i)**i)
+    sum += array[i-1]
+
+print(array)
+print(sum)
