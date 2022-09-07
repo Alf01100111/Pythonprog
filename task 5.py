559b0582c1fc057dd80d016529a2a11d8bba2fee
Андрей хухарев <alf.master.mail@gmail.com>
2022-09-07 22:14:23 +0300

    shuffle task 5

--- task 5.py
+++ task 5.py
@@ -7,3 +7,17 @@ for i in range(10):
     array.append(i)
 
 print(array)
+
+while True:
+    choice = input("перемешать массив y/n ")
+    if choice == 'y':
+        i = len(array) - 1
+        while i >= 1:
+            j = random.randrange(0, i+1)
+            tmp = array[j]
+            array[j] = array[i]
+            array[i] = tmp
+
+            i-=1
+        print(array)
+    else: break
