08e9f9db7f1a0ef24f19fb42b31324a88e1b968a
Андрей хухарев <alf.master.mail@gmail.com>
2022-09-14 19:57:58 +0300

    task 3

--- hw4/task 1.py
+++ hw4/task 1.py
@@ -7,13 +7,13 @@
 d = 0.001
 pii = 3.1415926535
 
-def num_after_dot(num :int) -> int:
+def num_after_dot(num :int) -> int:  #посчитаем сколько цифр оставим после запятой
     res = 0
     while num != 1:
         num *= 10
         res += 1
     return res
 
-res = str(round(pii, num_after_dot(d)+1))
+res = str(round(pii, num_after_dot(d)+1)) #округлим до нужной цифры + 1
 
-print(res[0:len(res)-1])
+print(res[0:len(res)-1]) #берем срез без последней неверно округленной цифры
